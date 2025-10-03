e01086c7316be142da751372332a1be22e284fda
ItsSilva <juanse909@hotmail.com>
2025-10-03 15:21:58 -0500

    Update: Save data variable

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,33 +1,75 @@
 import { useSelector, useDispatch } from "react-redux";
 import type { RootState } from "../redux/store";
-import { setNameState } from "../redux/slices/authSlice";
+import { setNameState, setDescriptionState } from "../redux/slices/authSlice";
 import { useState } from "react";
 
 const Login = () => {
   const [inputValue, setInputValue] = useState("");
+  const [inputDescription, setInputDescription] = useState("");
+
+  const [data, setData] = useState({
+    name: "",
+    description: "",
+  });
+
   console.log(inputValue);
-  
+
   //Traer valores del slice propiamente
   const name = useSelector((state: RootState) => state.auth.name);
+  console.log(name);
 
-  //Para lanzar las acciones que cambian los valores del slice
+  const description = useSelector((state: RootState) => state.auth.description);
+  console.log(description);
+
   const dispatch = useDispatch();
 
   const handleName = () => {
     dispatch(setNameState(inputValue));
   };
 
+  const handleDescription = () => {
+    dispatch(setDescriptionState(inputDescription)); 
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setData({ name: inputValue, description: inputDescription });
+    console.log({ name: inputValue, description: inputDescription });
+    setInputValue("");
+    setInputDescription("");
+  };
+
   return (
     <>
-      <h2>{name}</h2>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          setInputValue(e.target.value)
-        }
-      />
-      <button onClick={handleName}>Save name</button>
+      <h2>Nombre: {data.name}</h2>
+      <h2>Descripción: {data.description}</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Nombre"
+          value={inputValue}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setInputValue(e.target.value)
+          }
+        />
+        <input
+          type="text"
+          placeholder="Descripción"
+          value={inputDescription}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setInputDescription(e.target.value)
+          }
+        />
+        <input type="submit" value="Enviar" />
+      </form>
+      <button
+        onClick={() => {
+          handleName();
+          handleDescription();
+        }}
+      >
+        Guardar en Redux
+      </button>
     </>
   );
 };
